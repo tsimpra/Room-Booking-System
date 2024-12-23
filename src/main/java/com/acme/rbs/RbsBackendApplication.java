aa446b4ba3cdd9fc393ebc8491a21c5c1e63f658
vtsimpra <vtsimpra@netcompany.com>
2024-12-23 21:41:42 +0200

    initial commit

---
+++ src/main/java/com/acme/rbs/RbsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.rbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/rbs/RbsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.rbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
